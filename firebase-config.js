c1f06d28ff13ad001c33910f4b5701efb913e057
royalroushanroy1914168-cloud <royalroushanroy1914168@gmail.com>
2026-09-13 12:10:48 +0530

    Refactor Firebase configuration export
    Removed unused imports and initialization code for Firebase.

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDcPh8Htm7DtIgF5Ozf5oktHOH7IVyLhAU",
   authDomain: "asko-884e7.firebaseapp.com",
   projectId: "asko-884e7",
@@ -15,7 +7,3 @@ const firebaseConfig = {
   appId: "1:429787629679:web:8f144af364b481a26ee59a",
   measurementId: "G-BXB85V7HX5"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
